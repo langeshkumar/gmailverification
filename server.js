f056bc11c74f095a6ee2b2d6f5566f044d934293
langeshkumar <langeshkumar8@gmail.com>
2024-11-11 22:48:23 +0530

    database connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import gmaildatabase from "./lib/database.js";
 
 const app = express();
 const PORT = "3001";
@@ -8,6 +9,10 @@ app.get('/', (req, res) => {
     res.send({ message: "Server is running..!" });
 });
 
+// database connection
+gmaildatabase();
+
+// post connection 
 app.listen(PORT, () => {
     console.log(`Server is getting ready to http://localhost:${PORT}`);
 });
